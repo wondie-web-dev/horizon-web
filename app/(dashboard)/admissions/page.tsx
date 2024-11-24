40e371b9483ebc4b8f4eb8ba19f17904b45e5a92
Yekaalo <ymikea@gmail.com>
2024-11-24 05:02:15 -0800

    fixed tab issue and enhancements

---
+++ app/(dashboard)/admissions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdmissionsPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Admissions - Work in Progress</h1>
+    </div>
+  )
+}
+
+export default AdmissionsPage
